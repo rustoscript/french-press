ba60eec95303f324291aee49ffd5dc6af6d1300c
David Mally <djmally@gmail.com>
2015-10-04 21:10:19 -0400

    Add JsTrait impls & fix pub visibility on js_primitive types

--- src/js_types/js_primitive.rs
+++ src/js_types/js_primitive.rs
@@ -1,21 +1,30 @@
-mod js_obj;
-mod js_type;
-
 use std::string::String;
 use std::vec::Vec;
+use js_types::js_type::{JsTrait, JsT};
 
 // `undefined`
 pub struct JsUndef;
+impl JsTrait for JsUndef {}
+
 // `null`
 pub struct JsNull;
+impl JsTrait for JsNull {}
+
 // `bool`
-pub struct JsBool(bool);
+pub struct JsBool(pub bool);
+impl JsTrait for JsBool {}
+
 // `number`
-pub struct JsNum(f64);
+pub struct JsNum(pub f64);
+impl JsTrait for JsNum {}
+
 // `symbol`
-pub struct JsSym(String);
+pub struct JsSym(pub String);
+impl JsTrait for JsSym {}
+
 // `array`
-pub type JsArr<JsT> = Vec<Box<JsT>>;
+pub type JsArr<T> = Vec<T>;
+impl<T> JsTrait for JsArr<T> {}
 
 // `string`
 pub struct JsStr {
@@ -25,9 +34,11 @@ pub struct JsStr {
 impl JsStr {
     const MAX_STR_LEN: u64 = 9007199254740991; // 2^53 - 1
 
-    fn new(s: &str) -> JsStr {
+    pub fn new(s: &str) -> JsStr {
         assert!((s.len() as u64) < JsStr::MAX_STR_LEN);
         JsStr { text: s.to_string(), }
     }
 }
+
+impl JsTrait for JsStr {}
 
