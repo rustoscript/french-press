16cbcd00cbeb72a884e329464666f54299e89416
David Mally <djmally@gmail.com>
2015-10-04 21:10:55 -0400

    Use correct pub visibility on JsObj methods

--- src/js_types/js_obj.rs
+++ src/js_types/js_obj.rs
@@ -1,28 +1,17 @@
 use std::collections::hash_map::HashMap;
 use std::vec::Vec;
-use js_types::js_type::{JsType, JsT};
+use js_types::js_type::{JsTrait, JsT};
 
-pub type JsProto = Option<Box<JsObj>>;
-
-macro_rules! hash_map {
-    ( $( $k:expr => $v:expr),* ) => {{
-            let mut temp_hash_map = HashMap::new();
-            $(
-                temp_hash_map.insert($k, $v);
-            )*
-            temp_hash_map
-    }}
-}
 
 pub struct JsObj {
     proto: JsProto,
-    dict: HashMap<JsType, JsType>,
+    dict: HashMap<JsT, JsT>,
 }
 
-impl JsT for JsObj {}
+impl JsTrait for JsObj {}
 
 impl JsObj {
-    pub fn new(proto: JsProto, kv_pairs: Vec<(JsType, JsType)>) -> JsObj {
+    pub fn new(proto: JsProto, kv_pairs: Vec<(JsT, JsT)>) -> JsObj {
         let mut obj_map = HashMap::new();
         kv_pairs.into_iter().map(|(k,v)| obj_map.insert(k, v));
         JsObj {
