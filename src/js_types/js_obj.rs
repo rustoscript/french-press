7cb8a2712a3096e7e30ac415e2119b62a1791385
David Mally <djmally@gmail.com>
2015-10-04 21:11:07 -0400

    Add (failing) tests to js_obj

--- src/js_types/js_obj.rs
+++ src/js_types/js_obj.rs
@@ -20,3 +20,28 @@ impl JsObj {
         }
     }
 }
+
+pub type JsProto = Option<Box<JsObj>>;
+
+#[cfg(test)]
+mod test {
+    use js_types::js_obj::JsObj;
+    use js_types::js_type::JsT;
+    use js_types::js_primitive::{JsNum, JsStr};
+    #[test]
+    fn test_js_obj() {
+        let mut vec: Vec<(JsT, JsT)> = Vec::new();
+        for i in 0..10 {
+            let k = JsT::new(Box::new(JsNum(i as f64)));
+            let v = JsT::new(Box::new(JsStr::new(&format!("test{}", i))));
+            vec.push((k,v));
+        }
+        let o = JsObj::new(None, vec);
+        for (k, v) in o.dict {
+            assert!(k >= 0); // Problem: is this type erased at runtime?
+            assert!(k < 10);
+            assert!(v.starts_with("test"));
+        }
+    }
+
+}
