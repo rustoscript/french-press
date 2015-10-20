2fe75c554e61187e5b807470951ffec46bda90d7
David Mally <djmally@gmail.com>
2015-10-19 23:33:46 -0400

    Add UUID map for object IDs, but need to rework it to put it inside an arena

--- src/alloc/ref_manip.rs
+++ src/alloc/ref_manip.rs
@@ -1,10 +1,61 @@
-use js_types::js_type::{JsT, JsType};
+use js_types::js_type::{JsT, JsType, Marking};
 use std::vec::Vec;
 use std::collections::hash_set::HashSet;
+use std::collections::hash_map::HashMap;
 use std::collections::vec_deque::VecDeque;
 use uuid::Uuid;
 
-/// A graph node that stores a set of object UIDs, as well as a list of
+
+/// A map from uuids to references to objects. The interface to the garbage
+/// collector, this map gets updated whenever a GC cycle is performed. References
+/// that are no longer live may be dropped from the map.
+pub struct UuidMap<'t> {
+    inner: HashMap<Uuid, RefCell<JsT>>,
+}
+
+impl<'t> UuidMap<'t> {
+    pub fn new() -> UuidMap<'t> {
+        UuidMap {
+            inner: HashMap::new(),
+        }
+    }
+
+    pub fn from_jsts(jsts: Vec<&'t JsT>) -> UuidMap<'t> {
+        let mut tmp_map = HashMap::new();
+        jsts.iter().map(|j| tmp_map.insert(j.uuid, *j));
+        UuidMap {
+            inner: tmp_map,
+        }
+    }
+
+    pub fn insert_ref(&mut self, jst: &'t JsT) {
+        self.inner.insert(jst.uuid, jst);
+    }
+
+    pub fn drop_uuid(&mut self, uuid: Uuid) {
+        self.inner.remove(&uuid);
+    }
+
+    pub fn drop_ref(&mut self, jst: &'t JsT) {
+        self.inner.remove(&jst.uuid);
+    }
+
+    pub fn mark_uuid(&mut self, uuid: Uuid, marking: Marking) {
+        if let Some(jst) = self.inner.get_mut(&uuid) {
+            (*jst).gc_flag = marking;
+        }
+        //let jst_opt = self.inner.get_mut(&uuid);
+        //if let Some(jst) = jst_opt {
+        //    let ref mut jst = jst.clone();
+        //    jst.gc_flag = marking;
+        //    self.inner.insert(uuid, jst);
+        //}
+    }
+}
+
+
+
+/// A graph node that stores sets of object uuids, as well as a list of
 /// pointers to nodes it can flow to.
 /// A variable v is live on edge e if there is:
 ///     - a node n in the CFG s.t. use[n] contains v
