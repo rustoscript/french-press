a006f88ba20f66fbb694d2ce00d255700b673dc5
David Mally <djmally@gmail.com>
2015-10-20 16:14:21 -0400

    Make UuidMap map uuids -> &RefCell<JsT>s

--- src/alloc/ref_manip.rs
+++ src/alloc/ref_manip.rs
@@ -1,55 +1,50 @@
-use js_types::js_type::{JsT, JsType, Marking};
-use std::vec::Vec;
-use std::collections::hash_set::HashSet;
+use std::cell::RefCell;
 use std::collections::hash_map::HashMap;
+use std::collections::hash_set::HashSet;
 use std::collections::vec_deque::VecDeque;
+use std::vec::Vec;
+use js_types::js_type::{JsT, JsType, Marking};
 use uuid::Uuid;
 
 
 /// A map from uuids to references to objects. The interface to the garbage
 /// collector, this map gets updated whenever a GC cycle is performed. References
 /// that are no longer live may be dropped from the map.
-pub struct UuidMap<'t> {
-    inner: HashMap<Uuid, RefCell<JsT>>,
+pub struct UuidMap<'r> {
+    inner: HashMap<Uuid, &'r RefCell<JsT>>,
 }
 
-impl<'t> UuidMap<'t> {
-    pub fn new() -> UuidMap<'t> {
+impl<'r> UuidMap<'r> {
+    pub fn new() -> UuidMap<'r> {
         UuidMap {
             inner: HashMap::new(),
         }
     }
 
-    pub fn from_jsts(jsts: Vec<&'t JsT>) -> UuidMap<'t> {
+    /*pub fn from_jsts(jsts: Vec<RefCell<JsT>>) -> UuidMap<'r> {
         let mut tmp_map = HashMap::new();
-        jsts.iter().map(|j| tmp_map.insert(j.uuid, *j));
+        jsts.iter().map(|j| tmp_map.insert(j.borrow.uuid, j));
         UuidMap {
             inner: tmp_map,
         }
-    }
+    }*/
 
-    pub fn insert_ref(&mut self, jst: &'t JsT) {
-        self.inner.insert(jst.uuid, jst);
+    pub fn insert_ref(&mut self, jst: &'r RefCell<JsT>) {
+        self.inner.insert(jst.borrow().uuid, jst);
     }
 
     pub fn drop_uuid(&mut self, uuid: Uuid) {
         self.inner.remove(&uuid);
     }
 
-    pub fn drop_ref(&mut self, jst: &'t JsT) {
-        self.inner.remove(&jst.uuid);
+    pub fn drop_ref(&mut self, jst: RefCell<JsT>) {
+        self.inner.remove(&jst.borrow().uuid);
     }
 
     pub fn mark_uuid(&mut self, uuid: Uuid, marking: Marking) {
         if let Some(jst) = self.inner.get_mut(&uuid) {
-            (*jst).gc_flag = marking;
+            jst.borrow_mut().gc_flag = marking;
         }
-        //let jst_opt = self.inner.get_mut(&uuid);
-        //if let Some(jst) = jst_opt {
-        //    let ref mut jst = jst.clone();
-        //    jst.gc_flag = marking;
-        //    self.inner.insert(uuid, jst);
-        //}
     }
 }
 
