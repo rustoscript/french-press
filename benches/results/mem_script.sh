fe70ed8807872500f9912f0185a5da2d40e14c2a
David Mally <djmally@gmail.com>
2016-04-08 04:04:55 -0400

    Update mem benches

--- benches/results/mem_script.sh
+++ benches/results/mem_script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 benches=( init_only push_scope push_pop_no_gc push_pop_gc small_stack_alloc_no_gc small_heap_alloc_no_gc small_heap_alloc_no_gc_2 large_alloc_no_gc_flat_obj small_local_store shallow_load deep_load small_alloc_gc small_flat_heap_alloc_gc large_flat_alloc_gc leak_many )
 
@@ -12,6 +12,6 @@ for bench in "${benches[@]}"
 do
     mkdir -p $workdir/benches/results/space/$bench
     (cd $workdir/benches/mem &&
-     valgrind --tool=massif --time-unit=B --massif-out-file=$workdir/benches/results/space/$bench/$num cargo run -- $bench)
+     valgrind --tool=massif --time-unit=B --massif-out-file=$workdir/benches/results/space/$bench/$num ./target/debug/mem $bench)
     ms_print ./space/$bench/$num > ./space/$bench/pp_${num}
 done
